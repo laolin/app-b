00274e990b9a5300d6476bd49d41d0eda2c4a92b
LaoLin <hi@laolin.com>
2017-05-07 22:32:21 +0800

    <appb-ui-panel-cell-small> 组件

---
+++ app/modules/appb-ui/appb-ui-panel-cell-small.js
@@ -0,0 +1,26 @@
+'use strict';
+(function(){
+
+angular.module('appb')
+.component('appbUiPanelCellSmall',{
+    templateUrl: 'modules/appb-ui/appb-ui-panel-cell-small.template.html',  
+    bindings: {
+      text: '@',
+      url: '@',
+      icon: '@'
+    },
+    controller: ['$scope','$log','$timeout','$element',
+      function ($scope,$log,$timeout,$element){
+        var ctrl=this;
+        
+        ctrl.$onInit=function() {
+        }
+      }
+    ]
+})
+
+
+
+  
+//______________________________
+})();
