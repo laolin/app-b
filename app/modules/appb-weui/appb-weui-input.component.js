dc89536b5490f2e55fcff497ba39d32142c6a86c
LaoLin <hi@laolin.com>
2017-09-09 01:43:24 +0800

    <appb-weui-input>组件

---
+++ app/modules/appb-weui/appb-weui-input.component.js
@@ -0,0 +1,24 @@
+'use strict';
+(function(){
+
+angular.module('appb')
+.component('appbWeuiInput',{
+  templateUrl: 'modules/appb-weui/appb-weui-input.component.template.html',  
+  bindings: { 
+  
+  
+    formDefine:"<",
+  },
+  controller: ['$log','$timeout',
+    function ($log,$timeout){
+      var ctrl=this;
+      
+      
+      
+    }
+  ]
+})
+
+
+//___________________________________
+})();
