c719adbd7e6b5a6e765eb083e298be834527afc8
LaoLin <hi@laolin.com>
2017-09-16 20:33:19 +0800

    weui-pagination(空壳，未绑定变量和按钮操作)

---
+++ app/modules/appb-weui/appb-weui-pagination.component.js
@@ -0,0 +1,36 @@
+'use strict';
+(function(){
+
+angular.module('appb')
+.component('appbWeuiPagination',{
+    templateUrl: 'modules/appb-weui/appb-weui-pagination.component.template.html',  
+    bindings: {
+      tabs: '<'
+    },
+    controller: ['$scope','$log','$timeout','$location',
+      function ($scope,$log,$timeout,$location){
+        var ctrl=this;
+        
+        ctrl.$onInit=function() {
+        }
+        ctrl.goLink=function(a,index) {
+          for(var i=ctrl.tabs.length;i--;) {
+            ctrl.tabs[i]['active']= i==index;
+          }
+          if(a) {
+            if(typeof a == 'function') {
+              a(index);
+            } else {
+              $location.url(a);
+            }
+          }
+        }//end ctrl.goLink
+      }
+    ]
+})
+
+
+
+  
+//______________________________
+})();
