4d04d1b4740aa150926288ca9c0db872f28eb1f0
LaoLin <hi@laolin.com>
2017-09-24 22:56:16 +0800

    <fac-ui-copyfight>组件

---
+++ app/app-steefac/ui/fac-ui-copyright.component.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('steefac')
+.component('facUiCopyright',{
+  templateUrl: 'app-steefac/ui/fac-ui-copyright.component.template.html',
+  bindings: {
+  },
+  controller:['$log',
+	function ($log) {
+    
+  }]
+});
