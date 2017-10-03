ff784b762d37cf1948d7ab01730ee0238f2ff358
LaoLin <hi@laolin.com>
2017-10-03 13:30:41 +0800

    增加/sys-page.info页面，增加 AppbErrorInfo service

---
+++ app/modules/sys-page/sys-page.info.view.js
@@ -0,0 +1,31 @@
+'use strict';
+(function(){
+
+angular.module('appb')
+.config(['$routeProvider', function($routeProvider) {
+$routeProvider.when('/sys-page.info', {
+templateUrl: 'modules/sys-page/sys-page.info.view.template.html',
+controller: ['$scope','$log','$location','AppbData','AppbErrorInfo',
+function ($scope,$log,$location,AppbData,AppbErrorInfo) {
+  var appData=AppbData.getAppData();
+  appData.setPageTitle('系统消息');
+
+  var err=AppbErrorInfo.getLastInfo();
+  
+  $scope.type=err.type;
+  $scope.title=err.title;
+  $scope.content=err.content;
+  $scope.fn1=function(){$location.path(err.nextPage)};
+  
+  $scope.$on('$viewContentLoaded', function () {
+  });
+  $scope.$on('$destroy', function () {
+  });
+  
+
+}]
+})
+}]);
+
+//___________________________________
+})();
