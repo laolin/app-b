661bd4377aecce214d90436ab5ac86362041bcd1
LaoLin <hi@laolin.com>
2017-10-03 15:37:14 +0800

    编辑擅长构件的入口先占着

--- app/app-steefac/fac-detail/fac-detail.view.js
+++ app/app-steefac/fac-detail/fac-detail.view.js
@@ -8,6 +8,7 @@ templateUrl: 'app-steefac/fac-detail/fac-detail.template.html',
     'AppbData','FacDefine','FacMap','FacApi','FacUser',
   function ($scope,$http,$log,$location,
     AppbData,FacDefine,FacMap,FacApi,FacUser) {
+    var appData=AppbData.getAppData();
     var userData=AppbData.getUserData();
     if(! userData || !userData.token) {
       return $location.path( "/wx-login" ).search({pageTo: '/my'});;
@@ -17,36 +18,49 @@ templateUrl: 'app-steefac/fac-detail/fac-detail.template.html',
     
     var search=$location.search();
     var id=parseInt(search.id);
+    var myUid=appData.userData.uid;
     
-    $scope.detail={obj:0,init:0};
+    // 1,'steefac','detail'| 2,'stee_user','get_admin_of_fac'
+    $scope.isLoading=2;
+    
     $scope.FacUser=FacUser;
     $scope.fac={};
+    $scope.id=id;
+    $scope.canEdit=false;
     $scope.id=id;
 
     
     FacApi.callApi('steefac','detail',{id:id}).then(function(s){
       $log.log('detail',s);
       if(!s) {
-        $location.path( "/" );
-        return;
+        return appData.showInfoPage('参数错误','Err id: '+id,'/search')
       }
       FacDefine.formatObj(s);
-      $scope.detail.obj=s;
       $scope.fac=s;
-      $scope.detail.init= + new Date;
+      $scope.isLoading--;
       
       $scope.goodat=[];
-      s.goodat.split(',').forEach(function(val,ind){
-        $scope.goodat[ind]={text:val,icon:'check-circle'};
-      });
+      if(s.goodat) {
+        s.goodat.split(',').forEach(function(val,ind){
+          $scope.goodat[ind]={text:val,icon:'check-circle'};
+        });
+      }
       _get_admin_of_fac();
-    })
+    },function(e){
+      $log.log('detail Err',e);
+      return appData.showInfoPage('获取数据错误',e,'/search')
+    });
     function _get_admin_of_fac(){
+      FacUser.getMyData().then(function(s){
+        if(FacUser.isSysAdmin())$scope.canEdit=true;
+      },function(e){$log.log('Err:',e)});
       FacApi.callApi('stee_user','get_admin_of_fac',{facid:id}).then(function(s){
         $log.log('get_admin_of_fac',s);
+        $scope.isLoading--;
         if(s) {
           var uids=[];
           for(var i=s.length;i--; ) {
+            if(myUid==s[i].uid)$scope.canEdit=true;
             uids.push({uid:s[i].uid});
           }
           userData.requireUsersInfo(uids).then(function(){
