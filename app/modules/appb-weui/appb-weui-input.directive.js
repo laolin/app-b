248bc39b2ec45beaaa52c758b1272af0e785d775
LaoLin <hi@laolin.com>
2017-10-04 00:19:22 +0800

    改名

--- app/modules/appb-weui/appb-weui-input.directive.js
+++ app/modules/appb-weui/appb-weui-input.directive.js
@@ -4,10 +4,10 @@
 angular.module('appb')
 .directive('appbWeuiInput',function() {return {
   restrict: 'AE',
-  templateUrl: 'modules/appb-weui/appb-weui-input.component.template.html',  
+  templateUrl: 'modules/appb-weui/appb-weui-input.directive.template.html',  
   scope: { 
     inputDefine:"<appbInputDefine",
-  }
+  },
 }})
 
 
