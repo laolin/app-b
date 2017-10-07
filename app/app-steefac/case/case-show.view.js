5b6fcf751eeefd87d538e5963882681248bba241
LaoLin <hi@laolin.com>
2017-10-07 10:28:42 +0800

    显示案例页面上方显示 概况

--- app/app-steefac/case/case-show.view.js
+++ app/app-steefac/case/case-show.view.js
@@ -9,6 +9,7 @@ controller: ['$scope','$http','$log','$location',
 function ($scope,$http,$log,$location,
   AppbData,FacApi,FacUser,AppbFeedService) {
   var appData=AppbData.getAppData();
+  var userData=AppbData.getUserData();
         
   appData.setPageTitle('显示钢构厂业绩');
 
@@ -16,6 +17,8 @@ function ($scope,$http,$log,$location,
   $scope.id=parseInt(search.id);
   $scope.isLoading=1;
   
+  var myUid=appData.userData.uid;
+    
   if(!$scope.id) {
     return appData.showInfoPage('参数错误','Err id: '+$scope.id,'/search')
   }
@@ -30,13 +33,36 @@ function ($scope,$http,$log,$location,
     if(!s) {
       return appData.showInfoPage('参数错误','Err id: '+$scope.id,'/search')
     }
-    //$scope.det=s;
+    $scope.fac=s;
     $scope.title='显示'+s.name + '的业绩';
   },function(e){
     return appData.showInfoPage('发生错误',e+', id:'+$scope.id,'/search')
   });
 
-
+  $scope.canEdit=false;
+  _get_admin_of_fac();
+    function _get_admin_of_fac(){
+      FacUser.getMyData().then(function(s){
+        if(FacUser.isSysAdmin())$scope.canEdit=true;
+      },function(e){$log.log('Err:',e)});
+      FacApi.callApi('stee_user','get_admin_of_fac',{facid:$scope.id}).then(function(s){
+        $log.log('get_admin_of_fac',s);
+        $scope.isLoading--;
+        if(s) {
+          var uids=[];
+          for(var i=s.length;i--; ) {
+            if(myUid==s[i].uid)$scope.canEdit=true;
+            uids.push({uid:s[i].uid});
+          }
+          userData.requireUsersInfo(uids).then(function(){
+            $scope.uids=uids;
+            $scope.usersInfo=userData.usersInfo;          
+          });
+        }
+      },function(e){
+        $log.log('get_admin_of_fac Err',e);
+      });
+    }
   
 
 
