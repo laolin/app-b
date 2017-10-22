baf9315000452e0045daa3c46b96fcc25350f31b
LaoLin <hi@laolin.com>
2017-10-22 23:11:25 +0800

    增加 appbAPI 全局调用API 的接口。方便以后统一API调用

---
+++ app/modules/appb/appb-api.service.js
@@ -0,0 +1,48 @@
+'use strict';
+(function(){
+
+angular.module('appb')
+.factory('appbAPI', ['$log','$timeout','$http','$q','AppbData',
+function ($log,$timeout,$http,$q,AppbData){
+  var appData=AppbData.getAppData();
+
+  function appbApi(api,call,params) {
+    $log.log('appbApi.callApi ',api,call,params);
+    
+    var deferred = $q.defer();
+    
+    var url=appData.urlSignApi(api,call);
+    if(!url){
+      appData.requireLogin();//没有登录时 需要验证的 api 地址是空的
+      deferred.reject('needlogin:'+api+'.'+call);
+      return deferred.promise;
+    }
+
+    return $http.jsonp(url, {params:params})
+    .then(function(s){
+      if(s.data.errcode!=0) {
+        var err='Err:'+api+'.'+call+'('+s.data.msg+')';
+        $log.log(err);
+        deferred.reject(err);
+        return deferred.promise;
+      }
+
+
+      deferred.resolve(s.data.data);
+      return deferred.promise;
+    },function(e){
+      deferred.reject(e);
+      return deferred.promise;
+    });
+    
+  }//_api
+  
+  
+
+  return  appbApi;
+  
+}]);
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+})();
