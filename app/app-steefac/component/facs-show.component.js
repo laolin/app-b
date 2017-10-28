40a6e09a51bb7504c42fb6b611db2526425d9a5d
LaoLin <hi@laolin.com>
2017-10-28 11:48:26 +0800

    facs-show支持多类型

--- app/app-steefac/component/facs-show.component.js
+++ app/app-steefac/component/facs-show.component.js
@@ -21,14 +21,15 @@ function ($log,AppbData,FacUser,AppbAPI) {
   
   ctrl.$onInit=function(){
     $log.log('===============facsShow',ctrl.facIds);
-    getList(ctrl.facIds);
+    if(ctrl.facIds) getList(ctrl.facIds);
+    else ctrl.isLoading=0;
   }
   ctrl.$onChanges=function(chg){
 
   }
   
   function getList(facIds) {
-    return AppbAPI('steefac','li',{ids:facIds}).then(function(s){
+    return AppbAPI('steeobj','li',{type:ctrl.type,ids:facIds}).then(function(s){
       ctrl.isLoading=0;
       $log.log('===============steefac li ok',s);
       ctrl.facList=s;
