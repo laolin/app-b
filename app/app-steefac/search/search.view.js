858b285f972cd32128ab9e86bc768397e16d859d
linjianhong <linjianhong@github.com>
2017-12-30 21:09:05 +0800

    修改：搜索页面：允许搜索后退、保存搜索参数到hash

--- app/app-steefac/search/search.view.js
+++ app/app-steefac/search/search.view.js
@@ -14,14 +14,11 @@
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/search', config)
-      .when('/search/:id', config);
+      .when('/search/:ac', config);
   }]);
 
   function ctrl($scope, $routeParams,$log,$location,AppbData,FacSearch,FacMap) {
 
-    if($routeParams.id){
-
-    }
 
     $scope.appData = AppbData.getAppData();
 
@@ -31,44 +28,69 @@
     }
     $scope.FacSearch = FacSearch;
 
-    FacSearch.d = {
-      currentCity: "上海市杨浦区",
-      monthBetween: {from:'2017.12', to:'2018.3'}
-    }
-
-
-    var appData=AppbData.getAppData();
-
-    appData.setPageTitle('搜索钢构产能');
-    $scope.$on('$viewContentLoaded', function () {
-      if(!FacMap.searchMarkers.length)
-        FacMap.selPositionStart('search','选点搜周边');
-      FacSearch.showSearchMarkers(1,FacSearch.searchType);
-
-    });
-    $scope.$on('$destroy', function () {
-      FacSearch.showSearchMarkers(0,FacSearch.searchType);
-      FacSearch.hideInfoWindow();
-      FacMap.selPositionEnd();
-    });
-
     /**
      * 搜索
      */
-    $scope.searchResult = [];
-    $scope.searching = false;
-    $scope.search = FacSearch.options;
-    angular.extend(FacSearch.options, {
-      currentCity: "上海市杨浦区",
-      monthBetween: {from:'2017.12', to:'2018.3'},
-      level: "all",
-      orderBy: '按距离排序'
-    });
     $scope.research = function(searchType){
+      var param = {
+        searchType: searchType || FacSearch.searchType,
+        distSelect: $scope.search.distSelect,
+        level     : $scope.search.level     ,
+        orderBy   :$scope.search.orderBy    ,
+      };
+      if($location.$$path == "/search"){
+        $location.path('/search/searching').search(param);
+      }
+      else{
+        $location.replace('/search/searching').search(param);
+      }
+    }
+
+    if($routeParams.ac == 'searching'){
       $scope.searching = true;
-      FacSearch.startSearch(searchType || FacSearch.searchType).then(function(res){
+      $scope.searchResult = [];
+      $scope.search = FacSearch.options;
+      angular.extend(FacSearch.options, {
+        //currentCity: "上海市杨浦区",
+        //monthBetween: {from:'2017.12', to:'2018.3'},
+        distSelect: $location.$$search.distSelect,
+        level     : $location.$$search.level     ,
+        orderBy   : $location.$$search.orderBy   ,
+      });
+      FacSearch.startSearch($location.$$search.searchType || FacSearch.searchType, 'dontReLocation').then(function(res){
         $scope.searchResult = res;
-        console.log('搜索结果：', res);
+      });
+    }
+
+    else{
+      $scope.searching = false;
+      FacSearch.d = {
+        currentCity: "上海市杨浦区",
+        monthBetween: {from:'2017.12', to:'2018.3'}
+      }
+
+      var appData=AppbData.getAppData();
+
+      appData.setPageTitle('搜索钢构产能');
+      $scope.$on('$viewContentLoaded', function () {
+        if(!FacMap.searchMarkers.length)
+          FacMap.selPositionStart('search','选点搜周边');
+        FacSearch.showSearchMarkers(1,FacSearch.searchType);
+
+      });
+      $scope.$on('$destroy', function () {
+        FacSearch.showSearchMarkers(0,FacSearch.searchType);
+        FacSearch.hideInfoWindow();
+        FacMap.selPositionEnd();
+      });
+
+      $scope.searchResult = [];
+      $scope.search = FacSearch.options;
+      angular.extend(FacSearch.options, {
+        currentCity: "上海市杨浦区",
+        monthBetween: {from:'2017.12', to:'2018.3'},
+        level: "all",
+        orderBy: '按距离排序'
       });
     }
   }
