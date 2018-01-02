b5d3999e0eacb26f7cf3b64def520e7ecebbdf32
linjianhong <linjianhong@github.com>
2018-01-02 10:12:58 +0800

    添加：公司详情-有效产能组件

---
+++ app/app-steefac/component/fac-detail/fac-detail-capacity.component.js
@@ -0,0 +1,30 @@
+/**
+ * 公司详情-有效产能组件
+ * ver: 0.0.1
+ * build: 2018-01-02
+ * power by LJH.
+ */
+!(function (window, angular, undefined){
+  'use strict';
+
+  angular.module('steefac')
+  .component('facDetailCapacity',{
+    templateUrl: 'app-steefac/component/fac-detail/fac-detail-capacity.template.html',
+    bindings: {
+      fac: '<'
+    },
+    controller:['$scope', '$element', 'FacSearch', 'AppbData', ctrl]
+  });
+
+
+  function ctrl($scope, $element, FacSearch, AppbData) {
+    $scope.appData = AppbData.getAppData();
+    $scope.FacSearch = FacSearch;
+    var ctrl = this;
+    this.$onChanges=function(chg){
+      $scope.fac = ctrl.fac;
+      $scope.goodat = ((ctrl.fac||{}).goodat||'').split(',');
+      $scope.fee = JSON.parse((ctrl.fac||{}).fee||'{}');
+    }
+  }
+})(window, angular);
