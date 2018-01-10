9512376fcedfef2efa6665be1e17594529fa9718
linjianhong <linjianhong@github.com>
2018-01-10 17:45:08 +0800

    项目详情组件：当该项目无管理员时，才显示申请按钮

--- app/app-steefac/component/fac-detail/project-detail-profile.component.js
+++ app/app-steefac/component/fac-detail/project-detail-profile.component.js
@@ -13,20 +13,27 @@
     bindings: {
       fac: '<'
     },
-    controller:['$scope', '$element', 'FacSearch', 'AppbData', 'ProjDefine', 'FacUser', ctrl]
+    controller:['$scope', '$element', 'FacSearch', 'AppbData', 'ProjDefine', 'FacUser', 'AppbAPI', ctrl]
   });
 
 
-  function ctrl($scope, $element, FacSearch, AppbData, ProjDefine, FacUser) {
+  function ctrl($scope, $element, FacSearch, AppbData, ProjDefine, FacUser, AppbAPI) {
     $scope.appData = AppbData.getAppData();
     $scope.FacSearch = FacSearch;
     $scope.ProjDefine = ProjDefine;
+    $scope.canAddAdmin =  false;
     var ctrl = this;
     this.$onChanges=function(chg){
       $scope.fac = ctrl.fac || {};
+      if(!$scope.fac.id)return;
       // 是否管理员
       $scope.isSuperAdmin = FacUser.isAdmin();
       $scope.isThisAdmin = FacUser.canAdminObj('steeproj', $scope.fac.id);
+      AppbAPI('stee_user','get_admin_of_obj',{type:'steeproj', facid: $scope.fac.id}).then(function(json){
+        console.log('有管理员？', json);
+        console.log('isSuperAdmin', $scope.isSuperAdmin);
+        $scope.canAddAdmin = json.length == 0; // 为了一开始不显示数据？！
+      });
     }
   }
 })(window, angular);
