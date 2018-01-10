2bbbb99053c183043439f229325ee120154d189a
linjianhong <linjianhong@github.com>
2018-01-10 17:44:55 +0800

    公司详情组件：当该公司无管理员时，才显示申请按钮

--- app/app-steefac/component/fac-detail/fac-detail-profile.component.js
+++ app/app-steefac/component/fac-detail/fac-detail-profile.component.js
@@ -13,25 +13,32 @@
     bindings: {
       fac: '<'
     },
-    controller:['$scope', '$element', 'FacSearch', 'AppbData', 'FacUser', ctrl]
+    controller:['$scope', '$element', 'FacSearch', 'AppbData', 'FacUser', 'AppbAPI', ctrl]
   });
 
 
-  function ctrl($scope, $element, FacSearch, AppbData, FacUser) {
+  function ctrl($scope, $element, FacSearch, AppbData, FacUser, AppbAPI) {
     $scope.appData = AppbData.getAppData();
     $scope.FacSearch = FacSearch;
+    $scope.canAddAdmin =  false;
     var ctrl = this;
     this.$onChanges=function(chg){
       $scope.fac = ctrl.fac || {};
+      if(!$scope.fac.id)return;
       // 是否管理员
-      $scope.isSuperAdmin = FacUser.isAdmin();
+      $scope.isSuperAdmin = !!FacUser.isAdmin();
       $scope.isThisAdmin = FacUser.canAdminObj('steefac', $scope.fac.id);
+
       $scope.fee = (function(){
         let fees = Object.values(JSON.parse($scope.fac.fee || '{}'));
         let totle = 0;
         for(let v of fees) totle += +v;
         return Math.floor(totle / (fees.length || 1));
       })();
+      AppbAPI('stee_user','get_admin_of_obj',{type:'steefac', facid: $scope.fac.id}).then(function(json){
+        console.log('有管理员？', json);
+        $scope.canAddAdmin = json.length == 0; // 为了一开始不显示数据？！
+      });
     }
   }
 })(window, angular);
