9f55e1dbcb3ec1012b2fe1ba3244dcfde3e51b6b
linjianhong <linjianhong@github.com>
2018-01-23 23:27:35 +0800

    添加：SIGN.post 工厂

--- app/modules/appb/appb-api.service.js
+++ app/modules/appb/appb-api.service.js
@@ -1,5 +1,5 @@
 'use strict';
-(function(){
+(function(angular, window, undefined){
 
 angular.module('appb')
 .factory('AppbAPI', ['$log','$timeout','$http','$q','AppbData',
@@ -42,5 +42,86 @@ function ($log,$timeout,$http,$q,AppbData){
 }]);
 
 
+/**
+ * 签名 API 工厂
+ */
+angular.module('appb')
+.config(["$httpProvider", function($httpProvider){
+  /**
+   * 解决 post 问题
+   */
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+  var param = function(obj) {
+    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+    for(name in obj) {
+      value = obj[name];
+      if(value instanceof Array) {
+        for(i=0; i<value.length; ++i) {
+          subValue = value[i];
+          fullSubName = name + '[' + i + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value instanceof Object) {
+        for(subName in value) {
+          subValue = value[subName];
+          fullSubName = name + '[' + subName + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value !== undefined && value !== null)
+        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
+    return query.length ? query.substr(0, query.length - 1) : query;
+  };
+
+  // Override $http service's default transformRequest
+  $httpProvider.defaults.transformRequest = [function(data) {
+    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+  }];
+}])
+
+.factory('SIGN', ['$http', '$q', 'AppbData', function($http, $q, AppbData) {
+  var appData = AppbData.getAppData();
+  function jsonYes(json){
+    return json && +json.errcode === 0;
+  }
+
+  /**
+   * 用户 post 提交
+   * 自动签名
+   * 签名失败，跳转到登录页面
+   */
+  function post(api,call, data) {
+    var url = appData.urlSignApi(api, call);
+    if(!url){
+      appData.requireLogin();//没有登录时 需要验证的 api 地址是空的
+      return $q.reject('needlogin:'+api+'.'+call);
+    }
+    return $http.post(appData.urlSignApi(api,call), data)
+    .then( response => response.data )
+    .then(json => {
+      if(jsonYes(json)){
+        return $q.when(json);
+      }
+      else{
+        return $q.reject(json);
+      }
+    });
+  }
+
+  /**
+   * 暴露函数
+   */
+  return {
+    post : post
+  };
+
+}]);
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-})();
+})(angular, window);
