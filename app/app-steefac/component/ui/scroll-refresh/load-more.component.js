bc39bf4f545203506b45ee3b86ce5fd08ad76fbd
linjianhong <linjianhong@github.com>
2018-02-10 17:51:02 +0800

    下拉刷新组件：支持回调承诺

--- app/app-steefac/component/ui/scroll-refresh/load-more.component.js
+++ app/app-steefac/component/ui/scroll-refresh/load-more.component.js
@@ -16,21 +16,21 @@
         },
         templateUrl: 'app-steefac/component/ui/scroll-refresh/load-more.template.html',
         scope: {
-          listShow: '=', // 如果有该参数，表示父组件中，将使用这个参数显示列表，且此参数将由本组件从listBase自动添加数据
-          listBase: '=', // 当有 listShow 参数时，有用。全部显示时，即为显示本数组
+          listShow: '=?', // 如果有该参数，表示父组件中，将使用这个参数显示列表，且此参数将由本组件从listBase自动添加数据
+          listBase: '=?', // 当有 listShow 参数时，有用。全部显示时，即为显示本数组
           totle: '@', // 当没有 listShow 参数时，有用，将通知父组件，自行显示
           loaded: '@', // 当没有 listShow 参数时，有用，由父组件告知本组件已显示了几项，以判断是已显示到底了
           pageSize: '@',
-          onLoadMore: '&',
-          onLoadMoreNull: '&'
+          onLoadMore: '&'
         },
-        controller: ['$scope', ctrl]
+        controller: ['$scope', '$q', ctrl]
       };
     });
 
 
-  function ctrl($scope) {
+  function ctrl($scope, $q) {
     $scope.$watch('pageSize', pageSize => {
+      console.log('每页数量 = ', pageSize);
       page.pageSize = pageSize || 16;
     });
     $scope.$watch('totle', initAutoShow);
@@ -38,6 +38,7 @@
     $scope.$watch('listShow', initAutoShow);
     $scope.$watch('listBase', initAutoShow);
     function initAutoShow() {
+      console.log('数据改变 = ', $scope);
       if ($scope.listShow && $scope.listBase) {
         page.totle = $scope.listBase.length;
         page.loaded = $scope.listShow.length;
@@ -62,7 +63,10 @@
       $scope.checkTop = function (isTopMost) {
         $scope.isTopMost = isTopMost;
       }
+
+      $scope.loading = false;
       $scope.loadMore = function (event, top, isTopMost) {
+        if($scope.loading) return;
         // 父组件不显示，则由本组件进行默认显示
         if ($scope.listShow && $scope.listBase) {
           if ($scope.listShow.length >= page.totle) {
@@ -75,7 +79,15 @@
           page.loaded = $scope.listShow.length;
         }
         else{
-          $scope.onLoadMore(event, top, isTopMost);
+          $scope.loading = true;
+          $q.when($scope.onLoadMore(event, top, isTopMost))
+          .then(() => {
+            $scope.loading = false;
+          })
+          .catch( e => {
+            console.log("下拉刷新失败：", e);
+            $scope.loading = false;
+          })
         }
       }
     })();
