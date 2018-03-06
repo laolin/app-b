b37cffc7ad6bc044f77e737c07a5cd7c03c16dc3
linjianhong <linjianhong@github.com>
2018-03-06 17:01:54 +0800

    mini钢构厂组件：查看权限处理，必要时弹框

--- app/app-steefac/component/fac-detail/fac-detail-mini.js
+++ app/app-steefac/component/fac-detail/fac-detail-mini.js
@@ -15,11 +15,44 @@
       scope: {
         fac: "="
       },
-      controller: ['$scope', ctrl]
+      controller: ['$scope', '$element', '$location', 'FacUser', ctrl]
     }
   });
 
-  function ctrl($scope) {
+  function ctrl($scope, $element, $location, FacUser) {
+    var routers = {
+      steefac: '/fac-detail/',
+      steeproj: '/project-detail/',
+    }
+
+    $element.click( () => {
+      var fac = $scope.fac;
+      var type = 'steefac';
+      var path = routers[type];
+
+      console.log('DDDDDDDDDD', fac);
+      FacUser.getPageReadLimit(type, fac.id)
+      .then(limit =>{
+        if(limit == 'never'){
+          // 不受额度限制，直接打开
+          $location.path(path + fac.id).search({});
+          return;
+        }
+        FacUser.DjDialog.modal({
+          title: `今日额度 ${limit.max} 条，已用 ${limit.used} 条`,
+          body: `查看“${fac.name}”详情？`
+        }).then(()=>{
+          FacUser.applyReadDetail(type, fac.id).then(() =>{
+            $location.path(path + fac.id).search({c:1});
+          })
+        }, (e) => {
+          console.log('取消不看了');
+        });
+      })
+      .catch(info => {
+        FacUser.DjDialog.alert(info.text);
+      })
+    });
   }
 
 })(window, angular);
