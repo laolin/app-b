27637191a4382f3db6f56feddd74c6e9cb972c4f
linjianhong <linjianhong@github.com>
2018-03-28 16:49:25 +0800

    详情-简介组件：采用统一的额度提醒对话框

--- app/app-steefac/component/fac-detail/fac-mini-cell.js
+++ app/app-steefac/component/fac-detail/fac-mini-cell.js
@@ -20,38 +20,10 @@
   });
 
   function ctrl($scope, $element, $location, FacUser) {
-    var routers = {
-      steefac: '/fac-detail/',
-      steeproj: '/project-detail/',
-    }
-
     $element.click( () => {
       var fac = $scope.cell.fac;
       var type = $scope.cell.type;
-      var path = routers[type];
-
-      console.log('DDDDDDDDDD', fac);
-      FacUser.getPageReadLimit(type, fac.id)
-      .then(limit =>{
-        if(limit == 'never'){
-          // 不受额度限制，直接打开
-          $location.path(path + fac.id).search({});
-          return;
-        }
-        FacUser.DjDialog.modal({
-          title: `今日额度 ${limit.max} 条，已用 ${limit.used} 条`,
-          body: `查看“${fac.name}”详情？`
-        }).then(()=>{
-          FacUser.applyReadDetail(type, fac.id).then(() =>{
-            $location.path(path + fac.id).search({c:1});
-          })
-        }, (e) => {
-          console.log('取消不看了');
-        });
-      })
-      .catch(info => {
-        FacUser.DjDialog.alert(info.text);
-      })
+      FacUser.clickFac(fac, type);
     });
   }
 
