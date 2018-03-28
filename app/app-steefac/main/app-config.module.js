71f19cd462c88afb791879c3fa1710ecaf6bf0bf
linjianhong <linjianhong@github.com>
2018-03-28 08:56:19 +0800

    重写 $http 拦截

--- app/app-steefac/main/app-config.module.js
+++ app/app-steefac/main/app-config.module.js
@@ -3,53 +3,208 @@
   var theConfigModule = angular.module('app-config', ['app-site-config', 'dj-login'])
 
 
-  /** 签名配置 */
-  theConfigModule.config(['signProvider', 'SiteConfigProvider', 'UserTokenProvider', function (signProvider, SiteConfigProvider, UserTokenProvider) {
-
-    // 需要验证身份的 api 对应的 url, 已带验证身份用的 queryStr
-    function urlSign(api, call) {
-      var param = signParam(api, call || '');
-      if (!param) return '';
-      var url = SiteConfigProvider.apiRoot + api + "/" + call;
-      var queryString = Object.keys(param).map(k => {
-        return k + "=" + encodeURIComponent(param[k]);
-      })
-        .join('&');
-      return url + '?' + queryString;
-    }
-    /**
-     *  返回签名需要的参数
-     */
-    function signParam(api, call) {
-      var userToken = UserTokenProvider.reload()
-      var uid = userToken.data.uid;
-      var tokenid = userToken.data.tokenid;
-      var token = userToken.data.token;
-      if (!tokenid || !token || !api) {
-        return false;
-      }
-      var dt = new Date();
-      var timestamp = Math.round((dt.getTime() / 1000)) - 8 * 3600 - dt.getTimezoneOffset() * 60;//修正为东8区
-      //var api_signature = md5(api + call + uid + token + timestamp);
-      //return { uid, tokenid, timestamp, api_signature, sign };
-      var sign = md5(token + timestamp);
-      return { uid, tokenid, timestamp, sign };
-    }
-
-    signProvider.defaults.apiRoot = SiteConfigProvider.apiRoot;
-    signProvider.defaults.sign = function (api, call, SingedRequest) {
-      var calls = call.split('/')
-      var url = urlSign(api, calls[0]);
-      return SingedRequest(url, {});
-    }
-    // signProvider.defaults.onpost.push(function (api, call, data, signedUrl, postData) {
-    //   console.log('拦截api，模拟处理！', api, call, data, signedUrl, postData);
-    //   return { signedUrl, postData }
-    // });
+  theConfigModule.factory("SIGN", ["$http", function ($http) {
+    return {
+      post: function (api, call, data, config) {
+        return $http.post(api + '/' + call, data, config);
+      },
+      postLaolin: function (api, call, data) {
+        console.log("postLaolin", api + '/' + call)
+        return $http.post(api + '/' + call, data).then(json => json.data);
+      },
+    };
   }]);
 
-  theConfigModule.config(["$httpProvider", function ($httpProvider) {
-    $httpProvider.interceptors.push("http2sign");
-  }])
-  //___________________________________
+  theConfigModule.run(['$rootScope', '$http', '$q', 'sign', 'SiteConfig', 'UserToken', function ($rootScope, $http, $q, sign, SiteConfig, UserToken) {
+    sign.setApiRoot(SiteConfig.apiRoot);
+    sign.registerDefaultRequestHook((config, mockResponse) => {
+      return {
+        url: SiteConfig.apiRoot + config.url + "?" + UserToken.reload().signParamString(),
+        post: config.data
+      }
+    });
+  }]);
+
+
+
+  /** 用户模块 */
+  theConfigModule.run(['$rootScope', '$http', '$q', 'sign', function ($rootScope, $http, $q, sign) {
+
+    /**
+     * 所有用户权限
+     */
+    var theRights = [
+      { name: "推送项目给产能", fa: "window-restore", path: "/action/check-stock", color: "#f80" },
+      { name: "推送产能给项目", fa: "window-restore", path: "/action/check-stock", color: "#f80" },
+      { name: "更新项目", fa: "qrcode", path: "/action/new-obj-qrcode", color: "#c08" },
+      { name: "更新产能", fa: "edit", path: "", color: "#666" },
+      { name: "关闭项目", fa: "edit", path: "", color: "#666" },
+
+      { admin: 1, name: "用户管理", fa: "edit", path: "/roleright/list", color: "#080" },
+    ]
+
+    var USER = {
+      getMenu: function () {
+        return $http.post("app/verify_token").then(json => {
+          return sign.OK(json.datas, {
+            menu: userMenu
+          });
+        }).catch(e => {
+          return sign.error(-1, '登录失败', {
+            e,
+            menu: userMenu
+          });
+        })
+      },
+
+      "权限分组": function (rights) {
+        if (rights.indexOf("超级管理员") >= 0) {
+          return {
+            rightFull: theRights,
+            rightIcons: theRights,
+            rightUserIcons: theRights.filter(item => !item.admin),
+            rightAdminIcons: theRights.filter(item => item.admin)
+          }
+        }
+        return {
+          rightFull: theRights,
+          rightIcons: theRights.filter(item => rights.indexOf(item.name) >= 0),
+          rightUserIcons: theRights.filter(item => !item.admin && rights.indexOf(item.name) >= 0),
+          rightAdminIcons: theRights.filter(item => item.admin && rights.indexOf(item.name) >= 0)
+        }
+      },
+
+
+      /** 个人信息部分 */
+      me: {},
+      "刷新个人信息": function () {
+        return $http.post('app/me').then(json => {
+          USER.me = json.datas.me;
+          if (!angular.isArray(USER.me.rights)) USER.me.rights = [];
+          angular.extend(USER.me, USER["权限分组"](USER.me.rights));
+          return json;
+        })
+      },
+      "个人信息": function () {
+        if (USER.me && USER.me.uid) return sign.OK({ me: USER.me });
+        return USER["刷新个人信息"]();
+      },
+      "保存个人信息": function (attr) {
+        angular.extend(USER.me.attr || (USER.me.attr = {}), attr);
+        return $http.post('user/save_info', { attr })
+      },
+      //---- 个人信息部分 end --------
+
+
+      /** 微信数据部分 */
+      wxCache: [],
+      "微信数据": function (uids) {
+        var uidIsArray = angular.isArray(uids);
+        if (!uidIsArray) uids = [uids];
+
+        var uidAllCache = USER.wxCache.map(row => row.uid);
+        var uidInCache = uids.filter(uid => uidAllCache.indexOf(uid) >= 0);
+        var uidNotCache = uids.filter(uid => uidAllCache.indexOf(uid) < 0);
+
+        if (uidNotCache.length == 0) {
+          return sign.OK(uidIsArray ?
+            { list: USER.wxCache.filter(row => uids.indexOf(row.uid) >= 0) }
+            : { wx: USER.wxCache.find(wx => wx.uid = uids[0]) }
+          );
+        }
+
+        return $http.post('app/getWxInfo', { uid: uidNotCache }).then(json => {
+          var list = json.datas.list;
+          list.map(item => USER.wxCache.push(item));
+          return sign.OK(uidIsArray ?
+            { list: USER.wxCache.filter(row => uids.indexOf(row.uid) >= 0) }
+            : { wx: USER.wxCache.find(wx => wx.uid = uids[0]) }
+          );
+        })
+      },
+      //---- 微信数据部分 end --------
+
+      /** 未用 */
+      aaaa: function () {
+        return theRights;
+      },
+      bbbb: function () {
+        return theRights;
+      },
+    }
+
+
+    /**
+     * 用户模块
+     */
+    sign.registerHttpHook({
+      match: /^用户(\/(.*))?$/,
+      hookRequest: function (config, mockResponse, match) {
+        return mockResponse.resolve(match[2] ?
+          USER[match[2]](config.data) :
+          USER[config.data]()
+        );
+      }
+    });
+
+
+  }]);
+
+
+
+
+
+  if (0) {
+
+
+    /** 签名配置 */
+    theConfigModule.config(['signProvider', 'SiteConfigProvider', 'UserTokenProvider', function (signProvider, SiteConfigProvider, UserTokenProvider) {
+
+      // 需要验证身份的 api 对应的 url, 已带验证身份用的 queryStr
+      function urlSign(api, call) {
+        var param = signParam(api, call || '');
+        if (!param) return '';
+        var url = SiteConfigProvider.apiRoot + api + "/" + call;
+        var queryString = Object.keys(param).map(k => {
+          return k + "=" + encodeURIComponent(param[k]);
+        })
+          .join('&');
+        return url + '?' + queryString;
+      }
+      /**
+       *  返回签名需要的参数
+       */
+      function signParam(api, call) {
+        var userToken = UserTokenProvider.reload()
+        var uid = userToken.data.uid;
+        var tokenid = userToken.data.tokenid;
+        var token = userToken.data.token;
+        if (!tokenid || !token || !api) {
+          return false;
+        }
+        var dt = new Date();
+        var timestamp = Math.round((dt.getTime() / 1000)) - 8 * 3600 - dt.getTimezoneOffset() * 60;//修正为东8区
+        //var api_signature = md5(api + call + uid + token + timestamp);
+        //return { uid, tokenid, timestamp, api_signature, sign };
+        var sign = md5(token + timestamp);
+        return { uid, tokenid, timestamp, sign };
+      }
+
+      signProvider.defaults.apiRoot = SiteConfigProvider.apiRoot;
+      signProvider.defaults.sign = function (api, call, SingedRequest) {
+        var calls = call.split('/')
+        var url = urlSign(api, calls[0]);
+        return SingedRequest(url, {});
+      }
+      // signProvider.defaults.onpost.push(function (api, call, data, signedUrl, postData) {
+      //   console.log('拦截api，模拟处理！', api, call, data, signedUrl, postData);
+      //   return { signedUrl, postData }
+      // });
+    }]);
+
+    theConfigModule.config(["$httpProvider", function ($httpProvider) {
+      $httpProvider.interceptors.push("http2sign");
+    }])
+
+  }
 })(angular, window);
--- app/modules/appb/service/sign.service.js
+++ app/modules/appb/service/sign.service.js
@@ -43,242 +43,347 @@
   }]);
 
 
-  /** 签名后的数据类型 */
-  function SingedRequest(url, post, result) {
-    if (this instanceof SingedRequest) {
-      this.url = url;
-      this.post = post;
-      this.result = result;
+  /** 模拟 response 响应的数据类型 */
+  var CHttpMockResponse;
+  serviceModule.run(['$q', '$http', function ($q, $http) {
+    /** 为识别模拟服务器数据 */
+    CHttpMockResponse = function (data) {
+      this.data = data;
     }
-    else return new SingedRequest(url, post, result);
-  }
-  serviceModule.run(['$q', function ($q) {
-    SingedRequest.prototype = {
-      reject: function (result) {
-        this.result = $q.reject(result);
-        return this;
-      },
-      when: function (result) {
-        this.result = $q.when(result);
-        return this;
-      }
-    }
+    CHttpMockResponse.$q = $q;
+    CHttpMockResponse.$http = $http;
+    CHttpMockResponse.reject = function (data) {
+      return new CHttpMockResponse($q.reject(data));
+    };
+    CHttpMockResponse.resolve = function (data) {
+      return new CHttpMockResponse($q.when(data));
+    };
   }]);
 
 
   /**
    * 签名供应商
+   *
+   *
+   *
+   * 重设 apiRoot
+   * # Example
+   * 拦截后，$http.post('abc/def') 将被重定向到 http://mysite.com/api/abc/def
+   * ```js
+     app.run(['sign', function(sign){
+       sign.setApiRoot( 'http://mysite.com/api' );
+     }]);
+   * ```
+   *
+   *
+   * 拦截默认的 request
+   * # Example
+   * ```js
+      app.run(['sign', function(sign){
+        sign.registerDefaultRequestHook((url, post, mockResponse) => {
+          return {
+            url: 'http://mysite.com/api/' + url,
+            post
+          }
+        });
+      }]);
+   * ```
+   *
+   *
+   * 拦截默认的 response
+   * # Example
+   * ```js
+      app.run(['sign', function(sign){
+        sign.registerDefaultResponseHook((response) => {
+          return $q.when(response.data).then(json => {
+            if (json && +json.errcode === 0) return json;
+            return $q.reject(json);
+          })
+        });
+      }]);
+   * ```
+   *
+   *
+   *
+   * # 模拟服务器响应 mockResponse
+   * # Example
+   * ```js
+      app.run(['sign', function(sign){
+        sign.registerHttpHook( {
+          match: /^test\/(.*)/i,
+          hookRequest: function(config, mockResponse){
+            return mockResponse({
+              errcode: 0,
+              datas: data
+            });
+            // 或:
+            return mockResponse.resolve({
+              errcode: 0,
+              datas: data
+            });
+            // 或:
+            return mockResponse.reject('error msg');
+          }
+        });
+      }]);
+   * ```
+   * 或：
+   * # Example
+   * ```js
+      app.run(['sign', function(sign){
+        sign.registerHttpHook( {
+          match: /^test\/(.*)/i,
+          hookRequest: function(config, mockResponse){
+            return mockResponse({
+              errcode: 0,
+              datas: data
+            });
+            // 或:
+            return mockResponse.resolve({
+              errcode: 0,
+              datas: data
+            });
+            // 或:
+            return mockResponse.reject('error msg');
+          }
+        });
+      }]);
+   *
+   *
+   * # 对旧的请求，修改url, 修改提交的post数据, 修改响应的数据
+   * # Example
+   * ```js
+      app.run(['sign', function(sign){
+        sign.registerHttpHook( {
+          match: /^(old_api_name1|old_api_name2|old_api_name3)\/(.*)/i,
+          hookRequest: function(config, mockResponse){
+            return {
+              url: config.url + "?a=1&b=2",
+              post: {...config.data, oldSign: 'avsdf234sdfha'}
+            }
+          },
+          hookResponse: function(response, $q){
+            return $q.when(response.data).then(json => {
+              if (json && +json.errcode === 0) return {
+                errcode: 0,
+                datas: json.data
+              };
+              return $q.reject(json);
+            })
+          }
+        });
+      }]);
+   * ```
+   *
+   *
    */
   serviceModule.provider('sign', [function () {
 
-    /** url签名
-     * 用户可以拦截本服务，以自定义签名，或附加参数
-     * app.config(['signProvider', function(signProvider){
-     *   signProvider.defaults.sign = function(api, call, SingedRequest){
-     *     var your_url = "https:" + "//my.com/my_path/" + api + '/' + call + "?token=12345abc";
-     *     var your_post = {sign: 'abcd2d3d'}; // 这是要附加的数据
-     *     return new SingedRequest(your_url, your_post);
-     *   }
-     *   // jsonYes()、root 等，同上
-     * }])
+    /**
+     * apiRoot, 默认的服务端主目录
      */
-    var defaults = this.defaults = {
-      apiRoot: './',
-      sign: sign,
-      jsonYes: jsonYes,
-      before: before,
-      onpost: []
+    var theApiRoot = './';
+
+    function setApiRoot(apiRoot){
+      theApiRoot = apiRoot;
+      if (!/\/$/.test(theApiRoot)) theApiRoot += '/';
+    }
+
+    this.setApiRoot = setApiRoot;
+
+
+    /**
+     * 登记或注销 request/response 拦截
+     */
+    var theHttpHookRegistered = [];
+    var registerHttpHook =
+      this.registerHttpHook = (hook) => {
+        theHttpHookRegistered.indexOf(hook) < 0 && theHttpHookRegistered.push(hook);
+        return hook;
+      };
+    this.unRegisterHttpHook = (hook) => {
+      theHttpHookRegistered.splice(theHttpHookRegistered.indexOf(hook), 1);
     };
 
-    /** url签名
-     */
-    function sign(api, call, SingedRequest) {
-      return new SingedRequest(defaults.apiRoot + api + '/' + call, {});
-    }
 
-    /** 判断后端返回是否正确
+    /**
+     * 默认的 request 拦截器，当拦截器中未定义请求处理进，按此处理
+     * 用户可以自定义该拦截器, 多次定义，则最后一次有效
      */
-    function jsonYes(json) {
-      return json && +json.errcode === 0;
+    function defaultRequestHook(fn) {
+      defaultRequestHook.fn = fn;
     }
-
-    /** 后端返回后，进行预处理
-     * 返回 ===false ，将阻止后续处理，并自动抛出 stoped 拒绝
-     * 返回一个承诺，将重置原承诺
-     * 返回一个 !==false，将改写json为该值
-     */
-    function before(json) {
-      return json;
-      if (json.errcode === -1) {
-        // 跳转到登录页面
-        // 阻止后续操作
-        return false;
+    this.registerDefaultRequestHook = defaultRequestHook;
+    this.registerDefaultRequestHook((config, mockResponse) => {
+      var url = config.url;
+      var data = config.data;
+      if (!/^(http(s)?\:)?\/\//.test(url)) {
+        url = theApiRoot + url
       }
-    }
+      return { url, data }
+    });
 
-    /** post 拦截器
-     * 允许多次拦截，逐个执行
-     * 当返有一个回承诺，将该承诺作为模拟 api 的调用结果
-     * 当有一个返回 !==false，直接作为模拟 api 调用的承诺结果
+
+    /**
+     * 默认的 response 拦截器，当拦截器中未定义响应处理进，按此处理
+     * 用户可以自定义该拦截器, 多次定义，则最后一次有效
      */
-    var onpost = [
-      function (api, call, data, signedUrl, postData) { }
-    ];
+    function defaultResponseHook(fn) {
+      defaultResponseHook.fn = fn;
+    }
+    this.registerDefaultResponseHook = defaultResponseHook;
+    this.registerDefaultResponseHook((response, $q) => {
+      return $q.when(response.data).then(json => {
+        if (json && +json.errcode === 0) return json;
+        return $q.reject(json);
+      })
+    });
 
-
-    /** 请求前，进行预处理
+    /**
+     * 请求前，进行预处理
      * 返回 signed 类型数据 ，将阻止后续处理，并自动抛出 stoped 拒绝
      * 返回一个承诺，将重置原承诺
      * 返回一个 !==false，将改写json为该值
      */
-    function prePost(api, call, data) {
-      var sign = $get();
-      var signed = sign.sign(api, call, SingedRequest);
-      angular.extend(signed.post, data);
-      // 允许模拟 api 调用
-      for (var i in sign.onpost) {
-        var result = sign.onpost[i](api, call, data, signed.url, signed.post);
-        if (result) {
-          return signed.when(result);
+    function hookRequest(config) {
+      var hooked, match;
+      for (var i = theHttpHookRegistered.length; i--;) {
+        var hook = theHttpHookRegistered[i];
+        // 是否被拦截
+        if (hook.match && (match=config.url.match(hook.match))) {
+          hooked = hook.hookRequest(config, CHttpMockResponse, match);
+          if (hooked) {
+            return hooked;
+          }
         }
       }
-      if (!signed.url) {
-        return signed.reject('null require api.');
-      }
-      return signed;
+      return defaultRequestHook.fn(config, CHttpMockResponse);
     }
-    function $get() {
+
+
+    /**
+     * 响应后，进行预处理
+     * 返回 signed 类型数据 ，将阻止后续处理，并自动抛出 stoped 拒绝
+     * 返回一个承诺，将重置原承诺
+     * 返回一个 !==false，将改写json为该值
+     */
+    function hookResponse(response, $q) {
+      var hooked;
+      for (var i = theHttpHookRegistered.length; i--;) {
+        var hook = theHttpHookRegistered[i];
+        // 有拦截的，直接拦截，不再其它处理
+        if (hook.match && hook.hookResponse && response.config.urlBase.match(hook.match)) {
+          hooked = hook.hookResponse(response, $q);
+          if (hooked) return hooked;
+        }
+      }
+      return defaultResponseHook.fn(response, $q);
+    };
+
+    function OK(datas, other) {
+      return angular.extend({
+        errcode: 0,
+        datas
+      }, other);
+    }
+    function error(errcode, errmsg, other) {
+      return angular.extend({
+        errcode,
+        errmsg
+      }, other);
+    }
+
+    /**
+     * 暴露函数，以提供服务
+     */
+    this.$get = function () {
       return {
-        prePost: prePost,
-        apiRoot: defaults.apiRoot || './',
-        sign: defaults.sign || sign,
-        jsonYes: defaults.jsonYes || jsonYes,
-        before: defaults.before || before,
-        onpost: defaults.onpost || []
+        setApiRoot,
+        registerDefaultRequestHook: defaultRequestHook,
+        registerHttpHook,
+        OK,
+        error,
+        hookRequest: hookRequest,
+        hookResponse: hookResponse,
+        apiRoot: theApiRoot || './'
       }
     };
-    /**
-     * 暴露函数，以兑现拦截
-     */
-    this.$get = $get;
-
   }]);
 
-  /**
-   * 签名供应商
-   */
-  serviceModule.factory('SIGN', ['$q', '$http', 'sign', function ($q, $http, sign) {
-    /**
-     * 用户 post 提交
-     * 自动签名
-     * 签名失败，跳转到登录页面
-     */
-    function post(api, call, data) {
-      var signed = sign.prePost(api, call, data);
-      if (signed.result) {
-        return signed.result;
-      }
-      //alert(signed.url);
-      return $http.post(signed.url, signed.post, { signType: 'SIGN' })
-        .then(response => response.data)
-        .then(json => {
-          var result = sign.before(json);
-          if (result === false) {
-            return $q.reject('stoped');
-          }
-          return $q.when(result);
-        })
-        .then(json => {
-          if (sign.jsonYes(json)) {
-            return $q.when(json);
-          }
-          else {
-            return $q.reject(json);
-          }
-        });
-    }
-
-    return {
-      postLaolin: function(api, call, data){
-        //alert(api + ' / ' + call)
-        return post(api, call, data).then(json => json.data);
-      },
-      post
-    }
-
-  }]);
 
   /**
    * 签名拦截器
-   *注册方法：
-      app.config(["$httpProvider", function($httpProvider) {
-        $httpProvider.interceptors.push("http2sign");
-      }]);
    */
-  serviceModule.factory('http2sign', ['$q', '$rootScope', 'sign', function ($q, $rootScope, sign) {
-    /** 为识别模拟服务器数据 */
-    function CHttpResponse(data){
-      this.data = data;
-    }
+  serviceModule.factory('DJHttpHook', ['$q', '$rootScope', 'sign', function ($q, $rootScope, DJHttp) {
+
     return {
       request: function (config) {
-        // 只是增加后端请求主目录，而不需要签名
-        if (config.signType == 'single'){
-          config.url = sign.apiRoot + (/\/$/.test(sign.apiRoot)?'':'/') + config.url;
-          config.signType = '';
-          return config;
+        config.urlBase = config.url;
+        // 不是post，不拦截
+        if (config.method != "POST" || config.hook === false) return config;
+
+        var hook = config.hook || {};
+
+        if (hook.hookRequest === false) return config;
+
+        var hooked = DJHttp.hookRequest(config, CHttpMockResponse);
+        /** 如果签名后, 返回模拟服务器数据, 则不发出请求 */
+        if (hooked instanceof CHttpMockResponse) {
+          /** 先使用该模拟服务器数据拒绝，阻止发出请求，然后在响应错误拦截中，将重新兑现 */
+          return $q.reject(hooked);
         }
-        // 已用 SIGN 工厂签名过的, 或不是post, 不拦截
-        if (config.method != "POST" || config.signType=='SIGN') return config;
-        var api_call = config.url.split('/');
-        if(!api_call[0])api_call.shift();
-        var signed = sign.prePost(api_call[0], api_call[1], config.data);
-        if (signed.result) {
-          /** 使用模拟服务器数据，将在请求错误拦截中，重新兑现 */
-          return $q.reject(new CHttpResponse(signed.result));
+
+        // 只是进行 url 拦截
+        if (hook.hookRequest == 'url') {
+          config.url = hooked.url;
         }
-        //console.log('签名拦截器, 原url = ', config.url);
-        //console.log('签名拦截器, 原数据 = ', config.data);
-        config.data = signed.post;
-        config.url = signed.url;
-        //console.log('签名拦截器, config=', config);
+
+        // 只是进行 签名 拦截
+        else if (hook.hookRequest == 'sign') {
+          config.data = hooked.post;
+        }
+
+        // 进行 url 和 签名 拦截
+        else {
+          config.data = hooked.post;
+          config.url = hooked.url;
+        }
         return config;
       },
       requestError: function (rejection) {
         // do something on request error
-        console.log('签名拦截器, rejection=', rejection);
+        console.log('签名拦截器, 请求拒绝=', rejection);
         return $q.reject(rejection)
       },
       response: function (response) {
-        // 已用 SIGN 工厂签名过的, 或不是post, 不拦截
-        if (response.config.method != "POST" || response.config.signType == "SIGN") return response;
-        return $q.when(response.data)
-          .then(json => {
-            var result = sign.before(json);
-            if (result === false) {
-              return $q.reject('stoped');
-            }
-            return $q.when(result);
-          })
-          .then(json => {
-            if (sign.jsonYes(json)) {
-              return $q.when(json);
-            }
-            else {
-              return $q.reject(json);
-            }
-          });
+        // 不是post，不拦截
+        if (response.config.method != "POST" || response.config.hook === false) return response;
+
+        var hook = response.config.hook || {};
+
+        if (hook.hookResponse === false) return response;
+
+        // 进行响应拦截
+        return DJHttp.hookResponse(response, $q);
       },
       responseError: function (rejection) {
         /** 如果请求时, 使用模拟服务器数据, 则: 重新兑现 */
-        if(rejection instanceof CHttpResponse){
+        if (rejection instanceof CHttpMockResponse) {
           return $q.when(rejection.data);
         }
-        // do something on response error
-        console.log('签名拦截器, rejection=', rejection);
         return $q.reject(rejection);
       }
     };
   }]);
-  /** */
+
+
+  /**
+   * 注册拦截器，立即生效
+   */
+  serviceModule.config(["$httpProvider", function ($httpProvider) {
+    $httpProvider.interceptors.push("DJHttpHook");
+  }]);
+
 })(angular, window);
+
+
