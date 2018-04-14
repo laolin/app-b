f0ced1b4e0320d47dfd59a7a6815f3ae481d5dc1
linjianhong <linjianhong@github.com>
2018-04-14 14:20:30 +0800

    拨打电话组件：允许拦截点击事件

--- app/app-steefac/component/box/tel-mail/tel-box.js
+++ app/app-steefac/component/box/tel-mail/tel-box.js
@@ -4,24 +4,38 @@
  * build: 2018-01-05
  * power by LJH.
  */
-!(function (window, angular, undefined){
+!(function (window, angular, undefined) {
   'use strict';
 
   angular.module('steefac')
-  .component('telBox',{
-    templateUrl: 'app-steefac/component/box/tel-mail/tel-box.template.html',
-    bindings: {
-      val: '<'
-    },
-    controller:['$scope', '$element', 'AppbData', 'FacUser', ctrl]
-  });
+    .component('telBox', {
+      template: `
+        <a href="tel://{{$ctrl.val}}" class="flex flex-v-center tel-mal-box {{!$ctrl.val&&'disabled'}}">
+          <img ng-src="{{SiteConfig.assetsRoot}}/img/img-steefac/tel.png">
+          <span>拨打电话</span>
+        </a>
+      `,
+      bindings: {
+        val: '<',
+        click: "&"
+      },
+      controller: ['$scope', '$element', 'SiteConfig', '$http', '$q', ctrl]
+    });
 
 
-  function ctrl($scope, $element, AppbData, FacUser) {
-    $scope.appData = AppbData.getAppData();
-    $element[0].addEventListener('click', () => {
+  function ctrl($scope, $element, SiteConfig, $http, $q) {
+    $scope.SiteConfig = SiteConfig;
+    var defaultClick = () => {
       /* 记录用户点击 */
-      $scope.$emit('require-log-user-action', {ac: '点击电话', val: this.val});
+      $scope.$emit('require-log-user-action', { ac: '点击电话', val: this.val });
+    }
+    $element[0].addEventListener('click', () => {
+      var result = this.click({ val: this.val });
+      if (result) {
+        $q.when(result).then(defaultClick);
+      } else {
+        defaultClick();
+      }
     }, true);
   }
 })(window, angular);
