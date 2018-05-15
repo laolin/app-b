2f10215457326ab09432ee3e89fa2e6bf26c5f98
linjianhong <linjianhong@github.com>
2018-05-16 00:02:48 +0800

    对话框：使用 dj-component bower

--- app/app-steefac/main/http-factory/dialog.js
+++ app/app-steefac/main/http-factory/dialog.js
@@ -5,20 +5,23 @@
   /**
    * 显示对话框
    */
-  theConfigModule.run(['sign', 'DjDialog', function (sign, DjDialog) {
+  theConfigModule.run(['sign', 'DjPop', function (sign, DjPop) {
     sign.registerHttpHook({
       match: /^显示对话框\/(.*)$/,
       hookRequest: function (config, mockResponse, match) {
         var param = config.data;
         if (!angular.isArray(param) && angular.isObject(param)) {
-          if(param.body){
+          if(param.componentName){
+            param = [param.componentName, param.params, param.options]
+          }
+          else if(param.body){
             param = [param.body, param.title, param.options]
           }
           else{
             param = Object.keys(param).map(k => param[k]);
           }
         }
-        return mockResponse.resolve(DjDialog[match[1]].apply({}, param));
+        return mockResponse.resolve(DjPop[match[1]].apply({}, param));
       }
     });
   }]);
