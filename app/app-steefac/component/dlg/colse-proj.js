397c2cb9a2461d2d66d99e3dd1f5a50e60614402
linjianhong <linjianhong@github.com>
2018-05-16 11:36:10 +0800

    关闭项目对话框

---
+++ app/app-steefac/component/dlg/colse-proj.js
@@ -0,0 +1,78 @@
+
+!(function (window, angular, undefined) {
+  angular.module('steefac').component('dlgCloseProj', {
+    template: `
+    <djui-dialog param="dlgParam">
+      <djui-dialog-body>
+        <dj-form class="my-form" ng-if="$ctrl.close=='close'"
+          configs="configs"
+          on-form-values="formValueChange(value, item, valid, dirty)"
+          on-form-status="formStatusChange(item, valid, dirty)"
+        ></dj-form>
+        <div ng-if="$ctrl.close=='open'">
+          重新开放本项目？
+        </div>
+      </djui-dialog-body>
+    </djui-dialog>
+      `,
+    bindings: {
+      fac: '<',
+      close: '<',
+      backClose: '<',
+    },
+    controller: ['$scope', '$http', '$q', ctrl]
+  });
+
+  function ctrl($scope, $http, $q) {
+    var mode_list = [
+      { value: "pause", title: "暂停项目" },
+      { value: "deal", title: "成交关闭" },
+    ];
+    var mode = { name: 'mode', title: '关闭方式', type: 'dropdown', param: { valid: { require: true }, invalid: { required: "请选择" }, list: mode_list, placeholder: "关闭方式" } };
+    var theForm = {
+      pause: [
+        mode,
+        { name: 'name', title: '暂停原因', type: 'textarea', param: { valid: { require: true }, placeholder: "暂停原因" } },
+      ],
+      deal: [
+        mode,
+        { name: 'proj', title: '成交项目', type: 'input', param: { valid: { require: true }, placeholder: "成交项目" } },
+      ],
+      error: [
+        mode,
+      ],
+    }
+    $scope.configs = {
+      items: theForm.error,
+    };
+    $scope.dlgParam = {
+      beforeClose: (btnName) => {
+        if (btnName != "OK") return true;
+        if (!theData.valid && this.close != "open") return false;
+        return $http.post("sa_data/close_fac", { type: "steeproj", facid: this.fac.id, close: this.close });
+      },
+      backClose: this.backClose,
+    };
+    this.$onChanges = (changes) => {
+      if (changes.backClose) {
+        $scope.dlgParam.backClose = !!changes.backClose.currentValue;
+      }
+    }
+
+    /** 表单数据 */
+    var theData = {
+      valid: false,
+      value: {},
+    }
+    $scope.formValueChange = (value, item, valid, dirty) => {
+      theData.valid = valid;
+      theData.value = value;
+      if (item.name == 'mode') {
+        $scope.configs = angular.extend({}, $scope.configs, { items: theForm[value.mode || "error"] });
+      }
+    };
+    $scope.formStatusChange = (item, valid, dirty) => {
+      theData.valid = valid;
+    };
+  }
+})(window, angular);
