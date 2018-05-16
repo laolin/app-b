8781871c807f90a7eb5629d31c1a1b7e55096058
linjianhong <linjianhong@github.com>
2018-05-16 21:03:43 +0800

    tab组件：添加点击事件时传递参数

--- app/app-steefac/component/ui/tab-bar/tab-bar.component.js
+++ app/app-steefac/component/ui/tab-bar/tab-bar.component.js
@@ -4,11 +4,10 @@
  * build: 2017-12-20
  * power by LJH.
  */
-!(function (window, angular, undefined){
+!(function (window, angular, undefined) {
   'use strict';
 
-  angular.module('steefac')
-  .directive('tabBar', function(){
+  angular.module('steefac').directive('tabBar', function () {
     return {
       restrict: 'AE',
       templateUrl: 'app-steefac/component/ui/tab-bar/tab-bar.template.html',
@@ -19,18 +18,18 @@
         activeCss: '@',
         tabClick: '&',
       },
-      controller:['$scope', '$element', ctrl]
+      controller: ['$scope', '$element', ctrl]
     };
   });
 
 
   function ctrl($scope, $element) {
-    $scope.$watch("active", function(vNew){
+    $scope.$watch("active", function (vNew) {
       $scope.active = vNew || 0;
     });
-    $scope.clickTab = function(index){
+    $scope.clickTab = function (index, item) {
       $scope.active = index;
-      $scope.tabClick && $scope.tabClick({$n: index});
+      $scope.tabClick && $scope.tabClick({ $n: index, item: item });
     };
   }
 })(window, angular);
