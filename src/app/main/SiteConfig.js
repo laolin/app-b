b34c59928260fc20890ba532acabae592cf64446
linjianhong <linjianhong@github.com>
2018-05-24 21:54:26 +0800

    户登录票据供应商：优化写法

--- src/app/main/SiteConfig.js
+++ src/app/main/SiteConfig.js
@@ -7,9 +7,10 @@
 
 
   var theModule = angular.module('dj-service');
-  
+
   theModule.provider("SiteConfig", [function () {
     if (!/\/$/.test(theSiteConfig.apiRoot)) theSiteConfig.apiRoot = theSiteConfig.apiRoot + '/';
+    if (!/\/$/.test(theSiteConfig.apiRoot_unit)) theSiteConfig.apiRoot_unit = theSiteConfig.apiRoot_unit + '/';
     angular.extend(this, theSiteConfig);
     /**
      * 暴露数据
@@ -19,42 +20,42 @@
     }];
   }]);
 
-  
+
 
   /**
    * 基本的用户登录票据和签名方法, 
    */
   theModule.provider("UserToken", function () {
 
-    var UserToken = function(){
-      function UserToken(){
+    var UserToken = (function () {
+      function UserToken() {
         this.data = {};
         this.timestampOffset = 0; // 时间偏差
       }
       UserToken.prototype = {
-        load: () => {
+        load: function () {
           var k = theSiteConfig.localStorage_KEY_UserToken;
           this.data = JSON.parse(localStorage.getItem(k) || '{}');
           return this;
         },
         /** 保存到 */
-        save: (data) => {
+        save: function (data) {
           var k = theSiteConfig.localStorage_KEY_UserToken;
           localStorage.removeItem(k);
           localStorage.setItem(k, JSON.stringify(this.data = data));
           return this;
         },
-        hasToken: () => {
+        hasToken: function () {
           return this.data && this.data.tokenid && this.data.token;
         },
         /** 校准与服务器的时间偏差 */
-        adjustTimestamp: (timestampServer) => {
+        adjustTimestamp: function (timestampServer) {
           var dt = new Date();
           var timestampHere = Math.round((dt.getTime() / 1000));
           this.timestampOffset = timestampServer - timestampHere;
         },
         /** 用于 http 签名 */
-        signToken: () => {
+        signToken: function () {
           this.load()
           var tokenid = this.data.tokenid;
           var token = this.data.token;
@@ -71,23 +72,29 @@
       }
 
       return UserToken;
-    }
+    })();
 
     var userToken = new UserToken();
+    function reload(){
+      return userToken.load();
+    }
+    function save(data){
+      return userToken.save(data);
+    }
+
+    var theFactory = {
+      userToken,
+      reload,
+      save,
+    };
 
 
-
-    this.userToken = userToken;
-    this.reload = userToken.load;
     /**
      * 暴露数据
      */
+    angular.extend(this, theFactory);
     this.$get = function () {
-      return {
-        userToken,
-        reload: userToken.load,
-        save  : userToken.save
-      };
+      return theFactory;
     };
   })
 
