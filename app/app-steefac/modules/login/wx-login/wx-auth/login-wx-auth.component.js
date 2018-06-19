f3b0bfbdaf85ed804a76f8456fb94a07c41d2f9a
LinJP <hi@laolin.com>
2018-06-19 10:02:09 +0800

    加上随机数，否则微信不刷新

--- app/app-steefac/modules/login/wx-login/wx-auth/login-wx-auth.component.js
+++ app/app-steefac/modules/login/wx-login/wx-auth/login-wx-auth.component.js
@@ -82,6 +82,8 @@
       //bug: 首页不带#路径打开时，上一行location.hash是空 ，会导致使用#，进而导致不能跳到正确的路由
       var loginHash =  "#!" + "/wx-code-login"; //本项目应该总是使用 #! ，不会有使用 # 的情况
       var para1 = theSiteConfig.wx_app[appName].name;
+
+      //这里有加上随机数，'/'+ (+new Date) ，否则微信不刷新，会导致不会去真的登录
       var para2 = encodeURIComponent(btoa(redirect_page.split("#")[0] +'/'+ (+new Date) + loginHash));
       return `${theSiteConfig.wx_authApiBase}/${para1}/${para2}`;
     })(redirect_page);
